2619b3233c1088800283ec8c1d7ac59a14eb08d3
Victor Barres <barres@usc.edu>
2015-02-06 12:11:00 -0800

    Initial commit

---
+++ concept.py
@@ -0,0 +1,272 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 29 13:21:41 2014
+
+@author: Victor Barres
+
+Define semantic knowledge related classes for TCG1.0
+"""
+###############################################################################
+class SEM_REL:
+    """
+    Semantic Relation
+    
+    Data:
+        - type (INT): UNDEFINED=0, IS_A =1
+        - supMeaning (STR): Hypernym of subMeaning
+        - subMeaning (STR): Hyponym of supMeaning
+        
+    Note:
+        - Only IS_A hyponymic relation is defined in c++ code.
+    """
+    # Relation types
+    UNDEFINED = 0
+    IS_A = 1 # C++ code for TCG1.0 only handles hyponymy
+    
+    def __init__(self, aType = UNDEFINED, subMeaning = '', supMeaning = ''):
+        self.type = aType # Relation type
+        self.supMeaning = supMeaning # Superordinate meaning
+        self.subMeaning= subMeaning # Subordinate meaning
+    
+    def __eq__(self, other):
+        is_equal = (isinstance(other, self.__class__) and 
+            (self.type == other.type) and
+            (self.supMeaning == other.supMeaning) and 
+            (self.subMeaning == other.subMeaning))
+        return is_equal
+    
+    def __str__(self):
+        if self.type == SEM_REL.IS_A:
+            p = "%s IS_A %s" % (self.subMeaning, self.supMeaning)
+        else:
+            p = "type: %i, subMeaning: %s, supMeaning: %s" % (self.type, self.subMeaning, self.supMeaning)
+        return p
+    
+###############################################################################
+class SEM_NET:
+    """
+    Semantic network (acting as world knowledge)
+    
+    Data:
+        - relation ([SEM_REL]): List of semantic relations
+    
+    Notes: 
+        - The network is simply defined as a list of semantic relations.
+    """
+    def __init__(self, relations = []):
+        self.relations = relations
+    
+    def clear(self):
+        """
+        Clear all relations
+        """
+        self.relations = []
+    
+    def add_relation(self, sem_rel):
+        """
+        Add a relation to the semantic network
+        
+        Args:
+            sem_rel = SEM_REL
+        """        
+        # Check validity
+        if ((sem_rel.type == SEM_REL.UNDEFINED) or 
+            (sem_rel.supMeaning == '') or 
+            (sem_rel.subMeaning == '')):
+            return False
+        
+        # Check duplication
+        for r in self.relations:
+            if r == sem_rel:
+                return False
+        
+        # Add new relation
+        self.relations.append(sem_rel)
+        return True
+    
+    def root(self, concept, relType):
+        """
+        Get the farthest semantic root of a given concept (concept) for a given relation type (relType)
+        
+        Args:
+            - concept (str)
+            - relType (int)
+        
+        Notes: 
+            - Does not seem to be used anywhere in c++ code
+        """
+        return None
+    
+    def distance(self, supMean, subMean, relType = SEM_REL.IS_A, heuristic = False):
+        """
+        Return the distance in the semantic network between a concept (subMean) and an hypernym (supMean).
+        Return -1 if supMean is not a hypernym of subMean.
+        If heuristic = True: -> does not guarantee to find the shortest distance.
+        
+        Args:
+         - supMean (str)
+         - subMean (str)
+         - relType (int)
+         - heuristic (bool)
+        """
+        visited = [] # Explored elements of the semantic network
+        dist = 0
+        
+        return self._travel(supMean, subMean, relType, dist, heuristic, visited)
+    
+    def _travel(self, supMean, subMean, relType, dist, heuristic, visited):
+        
+        if supMean == subMean: # Match found
+            return dist
+        
+        minDist = -1
+        
+        for r in self.relations:
+            if r in visited:
+                continue
+            if (r.type != relType) or (r.subMeaning != subMean):
+                continue
+            
+            # Visit the current relation
+            visited.append(r)
+            
+            # Recursively travel
+            nextDist = self._travel(supMean, r.supMeaning, relType, dist+1, heuristic, visited)
+            
+            # Un-visit
+            if not(heuristic): # if heuristic: path does not have to be the shortest
+                visited.pop()
+                     
+            if nextDist >= 0:
+                if (minDist < 0) or (minDist > nextDist):
+                    minDist = nextDist
+                if minDist <= dist + 1: 
+                    break # assumed to be the possible minimum distance
+                
+                if heuristic:
+                    break # if heuristicL just find one possible path
+            
+        return minDist
+    
+    def __str__(self):
+        p = ''
+        p += '### SEMANTIC NETWORK ###\n\n'
+        for r in self.relations:
+            p += str(r) + '\n'
+        return p
+            
+            
+###############################################################################    
+class CONCEPT:
+    """
+    Concept (semantico-syntactic knowledge)
+    
+    Data:
+        - name (STR): Name of the concept.
+        - meaning (STR) : Meaning it carries.
+    """
+    SEMANTIC_NETWORK = None # A constant SEM_NET
+    
+    def __init__(self, name='', meaning=''):
+        self.name = name # Concept name
+        self.meaning = meaning # Concept meaning
+        
+    # Create concept (temporary)
+    def create(self, meaning='', concept=None):
+        """
+        Creates a concept from a meaning string or another concept.
+        (TEMPORARY)
+        """
+        # set field (temporary)
+        if meaning:
+            self.name = meaning
+            self.meaning = meaning
+            return True
+        
+        # set field (temporary)
+        if concept:
+            self.name = concept.name
+            self.meaning = concept.meaning
+            return True
+        
+        return False
+        
+    def match(concept1, concept2, inclusive = True):
+        """        
+        Check if two concept match. Case inclusive = False: concepts match only if they carry the same meaning.
+        Case inclusive = True: If a concept is tagged with + (e.g. ANIMAL+) it will match with any of its hyponyms.
+        Otherwise, concepts match if they carry the same meaning.
+        
+        Args:
+            - concept1 (CONCEPT)
+            - concept2 (CONCEPT)
+            - inclusive (BOOL)
+        
+        Notes:
+            - In the c++ version matching is boolean. No impact of distance on similarity.
+        
+        """
+        if not(CONCEPT.SEMANTIC_NETWORK):
+            return False
+        
+        mean1 = concept1.meaning
+        incl1 = mean1[-1] == '+'
+        if incl1:
+            mean1 = mean1[:-1]
+            
+        mean2 = concept2.meaning
+        incl2 = mean2[-1] == '+'
+        if incl2:
+            mean2 = mean2[:-1]
+        
+        # Forward direction
+        dist1 = CONCEPT.SEMANTIC_NETWORK.distance(mean1, mean2, heuristic = True)
+        if (inclusive and dist1>=0 and incl1):
+            dist1 = 0 # Inclusive
+        
+        # Backward direction
+        dist2 = CONCEPT.SEMANTIC_NETWORK.distance(mean2, mean1, heuristic = True)
+        if (inclusive and dist2>=0 and incl2):
+            dist2 = 0 # Inclusive
+        
+        if (dist1!=0) and (dist2!=0):
+         return False # Only meaning with distance 0 are accepted as matched
+        
+        #
+        # Check other fields too
+        2
+        return True
+
+###############################################################################
+if __name__=='__main__':
+    s1 = SEM_REL(SEM_REL.IS_A, 'cat', 'animal')
+    s2 = SEM_REL(SEM_REL.IS_A, 'dog', 'animal')
+    s3 = SEM_REL(SEM_REL.IS_A, 'animal', 'being')
+    
+    print s1
+    print s2
+    print s3
+    
+    semantic_network = SEM_NET(relations = [s1, s2])
+
+    semantic_network.add_relation(s3)
+    
+    print semantic_network
+    
+    concept1 = 'being'
+    concept2 = 'cat'
+    dist = semantic_network.distance(concept1, concept2, heuristic = False)
+    print "Distance between %s and %s = %i" %(concept1, concept2, dist)  
+
+    CONCEPT.SEMANTIC_NETWORK = semantic_network
+    
+    c1 = CONCEPT('c1', 'dog')
+    c2 = CONCEPT('c2', 'animal+')
+    
+    print CONCEPT.match(c1, c1)
+
+    
+    
+    
+    
+    
---
+++ data/README.txt
@@ -0,0 +1,42 @@
+Victor Barres
+TCG1.1
+
+TCG GRAMMAR: 
+
+1. Construction arerewritten as JSON objects. "test_data_cxn.json" gives a view of what TCG construction could look like in JSON format. The full translation of the TCG grammar into JSON is in progress in "TCG_grammar.json".
+
+2. In the new JSON format, the definitions is more verbose but clearer and similar to a feature-structure definition.
+	- Features: Name, Class, Preference, SemFrame, SynForm, SymLinks.
+With all those elements themeself feature structure or arrays of FS.
+The SemFrame is now defined more directly as a graph separating nodes and edges. The clarity gained with respect to graph definition is in a way counterbalanced by the lack of semantic transparency (in TCG1.0 the edges belonging to a predicate can be kept near the predicate...).
+The SynForm is defined as an array of element of type SLOT or PHON.
+SymLinks are clearly defined as a set of relations between nodes and slots (partial function).
+
+3. JSON validation schema for constructions defined in "cxn_schema.json"
+
+4. The phon feature of a PHON SynForm element of a construction might be better defined as an ARRAY of PHONEMES or SYLLABLES so that the length of an utterance can be easily captured.
+
+5. The preference feature for a construction is currently left optional (not all constructions have it). In TCG1.0 it is used to indicate the preference for some constructions over others in calculating suitability. It is set to 0 if not defined. I suggest that this should be a value assigned to the schema (participating in the initial activation level for an instance in addition with the quality of match). Later work will need to address how to better structure the grammatical LTM and have the preference and network weights adapt through use.
+
+TCG SEMANTICS:
+
+1. The semantic knowledge is rewritten in JSON format. This can keep the input in a form very close to that of TCG1.0.
+
+2. The main issue is that of how the knowledge should be devided.
+"SEMANTIC RELATION" are SemRep related knowledge ie what are the type of relations used in SemRep. It is used to match edge "concept" as this is called in TCG1.0: the type of semantic relations need to match during the invocation of a construction. I don't think that edges can match if they are in any other relations than symbolic equality (has to be a perfect match).
+"ENTITY" are object related conceptual knowledge. This is a part of the WK as conceptual knowledge.
+"ACTION" is a bit awkward since it defines simply two conceptal classes: TRANSITIVE and INTRANSITIVE actions. It is an important question to decide what type of knowledge this is. If it is linguistic knowledge, then this is a classic subcategorization of verbs. If it is actoin WK knowledge, is there a basis to suggest that transitive and intransitive are the right of classifying?
+"PROPERTY" this is fine. Note however that none of the properties are used to actually define elements of the ENTITY ontology (ie: SKY is BLUE etc..)!
+If the relations like "IN" are only defined in SEMANTIC RELATIONS then relations are missing from conceptual WK. Think about VISION system: it would be nice to be able to express things like SKY is above things, HUMAN have HANDS etc...
+
+3. In SandBox/TestWorldKnowledge I show how NetworkX can be used to implement WK as actual graph allowing for fast computation of things like: is_ascendant, is_descendant, distance between nodes (possibly weighted by edges weights) etc..
+
+4. It might be worth adding distinctions between: perceptual knowledge, conceptual knowledge, event knowledge [scripts/schemas]... This will play a role in TCG comprehension.
+
+5. For TCG comprehension, I would need a simple inference system: Bayesian net or something like that.
+
+N. go back to my quals and look at WK models in TD attention systems.
+
+TCG SCENE:
+
+
