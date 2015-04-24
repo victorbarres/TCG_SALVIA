325f636f6cab54b75ad1cfdb1daa295e44a71c39
Victor Barres <barres@usc.edu>
2015-04-24 14:47:38 -0700

    Subgraph isomorphism now returns both nodes and edges mapping

--- TCG_graph.py
+++ TCG_graph.py
@@ -16,15 +16,68 @@ def find_sub_iso(G, G_pat, node_match=None, edge_match=None): ### NEEDS TO RETUR
     Returns the list of all the graph isomorphisms between a subgraph of G and the graph pattern G_pat.
     G and G_pat should be NetworkX Digraphs.
     node_match and edge_match should be either "None" are networkx isomorphisms matching functions (categorical, numerical, or generic)
+    Each isomorphim is defined as a dictionary with keys "nodes" itself a dictionary mapping G_pat nodes to G nodes, and "edges" a dictionary mappign G_pat edges to 
+    G edges.
     """
     DiGM = isomorphism.DiGraphMatcher(G, G_pat, node_match=node_match, edge_match=edge_match)
 
     sub_iso = []
 
     my_iter  = DiGM.subgraph_isomorphisms_iter()
-    for iso in my_iter:
-        print iso
+    for mapping in my_iter:
+        iso = {"nodes":{}, "edges":{}}
+        for key in mapping:
+            iso["nodes"][mapping[key]] = key # reverse mapping for convenience (SemFrame -> SemRep)
+        
+        for edge in G_pat.edges(): # Add mapping between edges
+            iso["edges"][edge] = (iso["nodes"][edge[0]], iso["nodes"][edge[1]])
         sub_iso.append(iso)
     
     return sub_iso
 
+###############################################################################
+if __name__=="__main__":
+    import networkx as nx    
+    # Main graph
+    G = nx.DiGraph()
+    
+    G.add_node(0, attr=0)
+    G.add_node(1, attr=0)
+    G.add_node(2, attr=0)
+    G.add_node(3, attr=0)
+    
+    G.add_edge(0,1, attr=-1)
+    G.add_edge(1,2, attr=-1)
+    G.add_edge(2,3, attr=-1)
+    G.add_edge(3,0, attr=-1)
+    
+    nx.draw(G)
+    
+    # Graph pattern
+    G_pat= nx.DiGraph()
+    
+    G_pat.add_node("a", attr=0)
+    G_pat.add_node("b", attr=0)
+    G_pat.add_node("c", attr=0)
+    
+    G_pat.add_edge("a","b", attr=-1)
+    G_pat.add_edge("b","c", attr=-1)
+    
+    nx.draw(G_pat)
+    
+    # Categorical match functions
+    nm_cat = isomorphism.categorical_node_match("attr", 1)
+    em_cat = isomorphism.categorical_edge_match("attr", 1)
+    
+    print find_sub_iso(G, G_pat, node_match = nm_cat, edge_match=em_cat)
+    
+    # Numerical match functions
+    nm_num = isomorphism.numerical_node_match("attr", 0, atol=0.5, rtol=1e-05) # Matches if |x-y|<= atol + abs(y)*rtol
+    print find_sub_iso(G, G_pat, node_match = nm_num, edge_match=None)
+    
+    # Generic match functions
+    op = lambda x,y: x >= y
+    nm_gen = isomorphism.generic_node_match("attr", 0, op)
+    sub_iso = find_sub_iso(G, G_pat, node_match = nm_gen, edge_match=None)
+    
+    print sub_iso
