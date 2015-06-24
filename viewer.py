10126f52c6c0307278f201bf153ad52fb1520d35
Victor Barres <barres@usc.edu>
2015-06-24 16:38:04 -0700

    Reorganized knowlege. Concepts, percepts and conceptualizations are all stored within TCG_semantics.json. Rewrote the viewer method to generate dot file and graphviz visualizations for the three types of knowledges. Updatd the HTML to visualize.

--- viewer.py
+++ viewer.py
@@ -68,9 +68,8 @@ class TCG_VIEWER:
         shutil.copytree(self.data_path, self.viewer_path + self.tmp)
         self._create_cxn_imgs()
         dot_cpt = self._create_concept_img()
-        dot_sem= self._create_semrels_img()
         self._create_percept_img()
-        self._create_conceptualizer_img(dot_cpt, dot_sem)
+        self._create_conceptualizer_img(dot_cpt)
     
     def _create_cxn_imgs(self):
         """
@@ -202,59 +201,6 @@ class TCG_VIEWER:
         
         return dot_cpt
     
-    def _create_semrels_img(self):
-        """
-        Create graph image for the semantic relations knowledge.
-        Uses graphviz with pydot implementation.
-        """
-        import os, shutil
-        import subprocess        
-        import json
-        import pydot
-        
-        prog = 'dot'
-        file_type = 'svg'
-        
-        sem_folder = self.viewer_path + self.tmp + 'sem/'        
-        
-        if os.path.exists(sem_folder):
-            shutil.rmtree(sem_folder)
-        
-        os.mkdir(sem_folder)
-        
-        sem_file = 'TCG_semantics.json'
-        with open(self.viewer_path + self.tmp + sem_file, 'r') as f:
-            json_data = json.load(f)
-        
-        sem = json_data['SEM_RELATIONS']
-        edge_type = 'is_a'
-        dot_sem = pydot.Dot(graph_type='digraph')
-        dot_sem.set_rankdir('BT')
-        dot_sem.set_fontname('consolas')
-        font_size = '10'
-        color = 'black'
-        node_shape = 'box'
-        style = 'filled'
-        fill_color = 'white'
-        
-        def _add_rel(sup_node, sem_data, dot_sem):
-            for concept in sem_data:
-                dot_sem.add_node(pydot.Node(concept, label=concept, color=color, shape=node_shape, style=style, fillcolor=fill_color, fontsize=font_size))
-                if sup_node != None:
-                    dot_sem.add_edge(pydot.Edge(concept, sup_node, label=edge_type, fontsize=font_size))
-                
-                _add_rel(concept, sem_data[concept], dot_sem)
-        
-        _add_rel('SEMANTIC_RELATIONS', sem, dot_sem)
-        
-        file_name = sem_folder + 'TCG_semrels' + ".gv"
-        dot_sem.write(file_name)
-        # This is a work around becauses dot.write or doc.create do not work properly -> Cannot access dot.exe (even though it is on the system path)
-        cmd = "%s -T%s %s > %s.%s" %(prog, file_type, file_name, file_name, file_type)
-        subprocess.call(cmd, shell=True)
-        
-        return dot_sem
-    
     def _create_percept_img(self):
         """
         Create graph image for the percetual knowledge.
@@ -280,29 +226,32 @@ class TCG_VIEWER:
             json_data = json.load(f)
         
         per = json_data['PERCEPTUAL_KNOWLEDGE']
-        edge_type = 'is_a'
+        edge_type1 = 'is_a'
+        edge_type2 = 'token'
         dot_per = pydot.Dot(graph_type='digraph')
         dot_per.set_rankdir('BT')
         dot_per.set_fontname('consolas')
         font_size = '10'
         color = 'black'
         node_shape = 'box'
+        node_shape2 = 'octagon'
         style = 'filled'
         fill_color = 'white'
-        id_next = [0] # Just so that it's pass by name and not by value...
         
-        def _add_rel(sup_node, sup_id, per_data, dot_per, id_next):
-            for concept in per_data:
-                if concept!= 'FEATURE':  
-                    concept_id = id_next[0]
-                    id_next[0] +=1
-                    dot_per.add_node(pydot.Node(str(concept_id), label=concept, color=color, shape=node_shape, style=style, fillcolor=fill_color, fontsize=font_size))
-                    if sup_node != None:
-                        dot_per.add_edge(pydot.Edge(str(concept_id), str(sup_id), label=edge_type, fontsize=font_size))
-                    
-                    _add_rel(concept, concept_id, per_data[concept], dot_per, id_next)
+        def _add_rel(sup_node, per_data, dot_per):
+            for percept in per_data:
+                dot_per.add_node(pydot.Node(percept, label=percept, color=color, shape=node_shape, style=style, fillcolor=fill_color, fontsize=font_size))
+                if sup_node != None:
+                    dot_per.add_edge(pydot.Edge(percept, sup_node, label=edge_type1, fontsize=font_size))
+                
+                if isinstance(per_data[percept], list):
+                    for token in per_data[percept]:
+                        dot_per.add_node(pydot.Node(token, label=token, color=color, shape=node_shape2, style=style, fillcolor=fill_color, fontsize=font_size))
+                        dot_per.add_edge(pydot.Edge(token, percept, label=edge_type2, fontsize=font_size))
+                else:
+                    _add_rel(percept, per_data[percept], dot_per)
         
-        _add_rel('PERCEPTUAL_KNOWLEDGE', 0, per, dot_per, id_next)
+        _add_rel('PERCEPTUAL_KNOWLEDGE', per, dot_per)
         
         file_name = per_folder + 'TCG_percepts' + ".gv"
         dot_per.write(file_name)
@@ -312,7 +261,7 @@ class TCG_VIEWER:
         
         return dot_per
     
-    def _create_conceptualizer_img(self, dot_cpt, dot_sem):
+    def _create_conceptualizer_img(self, dot_cpt):
         """
         Create graph image for the conceputalizer.
         Uses graphviz with pydot implementation.
@@ -332,11 +281,11 @@ class TCG_VIEWER:
         
         os.mkdir(czer_folder)
         
-        czer_file = 'TCG_conceptualizer.json'
+        czer_file = 'TCG_semantics.json'
         with open(self.viewer_path + self.tmp + czer_file, 'r') as f:
             json_data = json.load(f)
         
-        czer = json_data['CONCEPTUALIZATIONS']
+        czer = json_data['CONCEPTUALIZATION']
         dot_czer = pydot.Dot(graph_type = 'digraph')
         dot_czer.set_rankdir('BT')
         dot_czer.set_fontname('consolas')
@@ -359,23 +308,13 @@ class TCG_VIEWER:
         cpt_sbg = _create_subgraph(dot_cpt)
         cluster_concepts.add_subgraph(cpt_sbg)
         
-        cluster_semrels = pydot.Cluster('semrels', label='semrels')
-        sem_sbg = _create_subgraph(dot_sem)
-        cluster_semrels.add_subgraph(sem_sbg)
-        
-        abstract_sbg = pydot.Subgraph()
-        abstract_sbg.add_subgraph(cluster_concepts)
-        abstract_sbg.add_subgraph(cluster_semrels)
-        
-        cluster_abstract = pydot.Cluster('abstract_knowlege', label='abstract_knowledge')
-        cluster_abstract.add_subgraph(abstract_sbg)
         cluster_percepts = pydot.Cluster('percepts', label='percepts')
         
-        dot_czer.add_subgraph(cluster_abstract)
+        dot_czer.add_subgraph(cluster_concepts)
         dot_czer.add_subgraph(cluster_percepts)
         
         for target in czer:
-            for source in czer[target]['TOKENS']:
+            for source in czer[target]:
                 cluster_percepts.add_node(pydot.Node(source, label=source, color=color, shape=node_shape, style=style, fillcolor=fill_color, fontsize=font_size))
                 dot_czer.add_edge(pydot.Edge(source, target, color=edge_color))
         
